0beda9ecdf3675d5775794e3be181ed837d70649
Joseph Boustany <jboustan@c1r1s5.42beirut.com>
2026-09-04 00:30:00 +0300

    ex01

--- Claude_Code/ex01/include/Form.hpp
+++ Claude_Code/ex01/include/Form.hpp
@@ -1,47 +1,53 @@
 #ifndef FORM_HPP
 #define	FORM_HPP
 
-#include<string>
-#include<exception>
-#include<ostream>
+#include <exception>
+#include <string>
+#include <ostream>
 
 class Bureaucrat;
 
 class Form
 {
-
 	public:
 
-		Form();
-		Form(std::string const& name, int const gradeSign, int const gradeExec);
-		Form(Form const& other);
-		Form& operator=(Form const& other);
-		~Form();
+	Form();
+	Form(int gradeToSign, int gradeToExec, std::string const& name);
+	Form(Form const& other);
+	Form& operator=(Form const& other);
+	~Form();
 
-		std::string getName() const;
-		const int getGradeSign() const;
-		const int getGradeExec() const;
+	std::string const& getName() const;
+	int getGradeToSign() const;
+	int getGradeToExecute() const;
+	bool getIsSigned() const;
+
+	void beSigned(Bureaucrat const& bureaucrat);
+
+	class GradeTooLowException : public std::exception
+	{
+		public:
+			virtual const char* what() const throw(); 
+
+	};
 
 		class GradeTooHighException : public std::exception
-		{
-			public:
-				virtual const char* what() const throw();
-		};
+	{
+		public:
+			virtual const char* what() const throw(); 
 
-		class GradeTooLowException : public std::exception
-		{
-			public:
-				virtual const char* what() const throw();
-		};
+	};
 
 	private:
-		const std::string _name;
-		bool _isSigned;
-		const int _gradeSign;
-		const int _gradeExec;
+
+	const std::string _name;
+	bool _isSigned;
+	const int _gradeToSign;
+	const int _gradeToExecute;
 
 };
 
 std::ostream& operator<<(std::ostream& os, const Form& form);
+
 
 #endif
